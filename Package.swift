c803b1a7770949df6820874b41edc3c5e33c6698
Alejandro Sanabria <alesanabriav@gmail.com>
2020-01-13 23:51:18 -0500

    up version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .library(name: "SwiftWebVTT", targets: ["SwiftWebVTT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/alesanabria/SwiftWebVTT", from: "1.0.0"),
+        .package(url: "https://github.com/alesanabria/SwiftWebVTT", from: "1.0.1"),
     ],
     targets: [
         .target(name: "SwiftWebVTT", dependencies: ["Utility"]),
